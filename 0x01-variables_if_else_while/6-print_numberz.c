70c9959107d118cf834f0675ccdf031c01b52cfc
AngeSol <solangeetse70@gmail.com>
2022-09-21 09:58:40 -0700

    Task6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 /**
- * main - Entry point
+ * main - prints the numbers since 0 to 9
  *
- * Return: Always 0 (Success)
+ * Return: Always(Sucess)
  */
 int main(void)
 {
-	int d;
+	int n;
 
-	for (d = '0'; d <= '9'; d++)
-		putchar(d);
-		putchar('\n');
+	for(n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
 
 	return (0);
 }
