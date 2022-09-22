0c6350fbda04c4bcd3b9e4b0b6bec1b7c2e6f620
AngeSol <solangeetse70@gmail.com>
2022-09-22 06:07:42 -0700

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,13 +8,13 @@ void times_table(void)
 {
 	int rw, col, d;
 
-	for (rw = 0; rw <= 9; rw++)
+	for (rw = 0; rw <= 10; rw++)
 	{
 		_putchar('0');
 		_putchar(',');
 		_putchar(' ');
 
-		for (col = 1; col <= 9; col++)
+		for (col = 1; col <= 10; col++)
 		{
 			d = (rw * col);
 
@@ -29,6 +29,5 @@ void times_table(void)
 			}
 			_putchar(' ');
 		}
-		_putchar(' ');
 	}
 }
