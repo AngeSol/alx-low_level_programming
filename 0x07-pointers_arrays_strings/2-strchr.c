2d9ada8a123470df8317e5ae9cedc464527da025
AngeSol <solangeetse70@gmail.com>
2022-09-26 04:11:47 -0700

    task2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,19 +3,19 @@
 /**
  * _strchr -> string character
  * @s: string given
- * @s: another char
+ * @c: another char
  * Return: a string
  */
 char *_strchr(char *s, char c)
 {
 	int a = 0, b;
-	
+
 	while (s[0])
 		a++;
 	for (b = 0; b < a; b++)
 	{
 		if (c == s[b])
-			s+= b;
+			s += b;
 		return (s);
 	}
 	return ('\0');
