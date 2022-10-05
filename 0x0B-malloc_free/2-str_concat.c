f65f2b26fd85d4b410d39c38bd5ff7e36d028648
AngeSol <solangeetse70@gmail.com>
2022-10-04 23:23:12 -0700

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -32,7 +32,7 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 		s1 = "";
 
-	if (s2 == ULL)
+	if (s2 == NULL)
 		s2 = "";
 
 	size = (_strlen(s1) + _strlen(s2) + 1);
@@ -41,7 +41,7 @@ char *str_concat(char *s1, char *s2)
 
 			if (dst == 0)
 			{
-			retur, (NULL);
+			return (NULL);
 			}
 
 			for (i = 0; *(s1 + 1) != '\0'; i++)
