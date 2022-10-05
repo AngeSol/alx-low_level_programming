76410ac6faa42be85bbf29f828e43a13137964a9
AngeSol <solangeetse70@gmail.com>
2022-10-04 23:36:07 -0700

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -41,10 +41,10 @@ char *str_concat(char *s1, char *s2)
 
 			if (dst == 0)
 			{
-			return (NULL);
+				return (NULL);
 			}
 
-			for (i = 0; *(s1 + 1) != '\0'; i++)
+			for (i = 0; *(s1 + i) != '\0'; i++)
 				*(dst + i) = *(s1 + i);
 
 			for (j = 0; *(s2 + j) != '\0'; j++)
